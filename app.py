51b731864ee544ec0fda93358374c29400bd7308
Aditya Maulana <adityamaulanaaaa@gmail.com>
2024-06-09 09:13:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,43 @@
-from flask import Flask, render_template, request
 import streamlit as st
-
-app = Flask(__name__)
-app.static_folder = 'tatic'
-
-if __name__ == "__main__":
-    app.run(port=5000)
-
 import nltk
-nltk.download('popular')
-from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
-
 from keras.models import load_model
-model = load_model('model/models.h5')
 import json
 import random
+
+# Pastikan download nltk dilakukan sekali saja
+nltk.download('popular', quiet=True)
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+# Load model dan data
+model = load_model('model/models.h5')
 intents = json.loads(open('model/data.json').read())
-words = pickle.load(open('model/texts.pkl','rb'))
-classes = pickle.load(open('model/labels.pkl','rb'))
+words = pickle.load(open('model/texts.pkl', 'rb'))
+classes = pickle.load(open('model/labels.pkl', 'rb'))
 
 def clean_up_sentence(sentence):
-    # tokenize the pattern - split words into array
     sentence_words = nltk.word_tokenize(sentence)
-    # stem each word - create short form for word
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
-
 def bow(sentence, words, show_details=True):
-    # tokenize the pattern
     sentence_words = clean_up_sentence(sentence)
-    # bag of words - matrix of N words, vocabulary matrix
     bag = [0]*len(words)  
     for s in sentence_words:
-        for i,w in enumerate(words):
+        for i, w in enumerate(words):
             if w == s: 
-                # assign 1 if current word is in the vocabulary position
                 bag[i] = 1
                 if show_details:
-                    print ("found in bag: %s" % w)
-    return(np.array(bag))
+                    print("found in bag: %s" % w)
+    return np.array(bag)
 
 def predict_class(sentence, model):
-    # filter out predictions below a threshold
-    p = bow(sentence, words,show_details=False)
+    p = bow(sentence, words, show_details=False)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
-    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
-    # sort by strength of probability
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
@@ -62,7 +48,7 @@ def getResponse(ints, intents_json):
     tag = ints[0]['intent']
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
-        if(i['tag']== tag):
+        if i['tag'] == tag:
             result = random.choice(i['responses'])
             break
     return result
@@ -72,23 +58,11 @@ def chatbot_response(msg):
     res = getResponse(ints, intents)
     return res
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+# Streamlit UI
+st.title("Chatbot")
+st.write("Ask me anything!")
 
-@app.route("/get")
-def get_bot_response():
-    userText = request.args.get('msg')
-    return chatbot_response(userText)
-
-if __name__ == "__main__":
-    if st._is_running_with_streamlit:
-        st.title("Chatbot")
-        user_input = st.text_input("You:")
-        if st.button("Send"):
-            response = chatbot_response(user_input)
-            st.write("Bot:", response)
-    else:
-        app.run()
-
-        
+user_input = st.text_input("You:")
+if st.button("Send"):
+    response = chatbot_response(user_input)
+    st.write("Bot:", response)
